b6943511d7a006512cf9ab4169c06a9ccdd9fef3
Jorlano D S Pereira <souojor@gmail.com>
2024-07-25 13:12:52 -0300

    product e2e

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,11 +1,14 @@
 import express, { Express } from "express"
 import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
+import ProductModel from "../product/repository/sequelize/product.model";
 import { customerRoute } from "./routes/customer.route";
+import { productRoute } from "./routes/product.route";
 
 export const app:Express = express();
 app.use(express.json());
 app.use("/customer", customerRoute);
+app.use("/product", productRoute);
 
 export let sequelize: Sequelize;
 
@@ -17,7 +20,7 @@ async function setupDb() {
         //sync: { force: true }
     });
 
-    sequelize.addModels([CustomerModel]);
+    sequelize.addModels([CustomerModel, ProductModel]);
     await sequelize.sync();
 }
 
